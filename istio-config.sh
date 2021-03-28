897cb981598e5374f945410a16acd877aac41021
loganrobertclemons <loganrobertclemons@gmail.com>
2021-03-28 15:59:50 -0500

    update istioconfig and add external-dns ns

--- istio-config.sh
+++ istio-config.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-kubectl label namespace istio-system istio-injection=enabled
-
-istioctl manifest generate -- set values.global.mtls.enabled=false \
-    --set values.kiali.enabled=false \
-    --set values.tracing.enabled=false \
-    --set values.tracing.provider=zipkin \
-    --set values.global.controlPlaneSecurityEnabled=true \
-    --set values.istiocoredns.enabled=true \
+istioctl manifest generate --set unvalidatedValues.global.mtls.enabled=true \
+    --set unvalidatedValues.security.selfSigned=true \
     --set values.global.proxy.resources.limits.cpu=250m \
     --set values.global.proxy.resources.limits.memory=256Mi \
-    --set values.gateways.istio-ingressgateway.serviceAnnotations."service\.beta\.kubernetes\.io/aws-load-balancer-internal"="0\.0\.0\.0/0" \
+    --set unvalidatedValues.gateways.istio-ingressgateway.enabled=true \
+> ./istio-manifest.yaml
